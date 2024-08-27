fa3a9fcd97e4d8642be37a985b2482e0727f3d45
Thilina Madushan <satmadushan97@gmail.com>
2024-08-27 16:46:10 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Numerics;
+
+public delegate int myDeligate(int x, int y);
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        Program p = new Program();
+        myDeligate calculator;
+
+        calculator = p.addition;
+        Console.WriteLine("Addition is " + p.addition(5,6));
+
+        calculator = p.multiplication;
+        Console.WriteLine("Multiplication is " + p.multiplication(5,4));
+    }
+
+    public int addition (int x, int y)
+    {
+        return x + y;
+    }
+
+    public int multiplication (int x, int y)
+    {
+        return x * y;
+    }
+}
